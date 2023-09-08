8e396b9c451b6b70bb031a11435361d22043ba05
Daniel Vega <vegad8510@gmail.com>
2023-09-08 00:23:10 -0600

    Cambios - Daniel

--- Proyecto.py
+++ Proyecto.py
@@ -7,4 +7,8 @@ print ("Voy a modificar a partir de esta línea, atte: Aranza")
 print ("Archivo modificado nuevamente")
 print (" C: ")
 
+print ("Voy a modificar a partir de esta línea, atte: Daniel")
+print ("Archivo modificado una vez más")
+print (" C: ")
+
  
