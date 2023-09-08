d76c8a4b5a5859d09d93373b7a35ecb82563d798
marco2220x <112792541+marco2220x@users.noreply.github.com>
2023-09-08 16:10:36 -0600

    Update Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,12 @@
+print ("Voy a modificar a partir de esta línea, Evelyn")
+print ("Archivo Modificado")
+print (" [: ")
+  
+
+print ("Voy a modificar a partir de esta línea, atte: Aranza")
+print ("Archivo modificado nuevamente")
+print (" C: ")
+
+print ("Voy a modificar a partir de esta línea, atte: Daniel")
+print ("Archivo modificado una vez más")
+print (" C: ")
