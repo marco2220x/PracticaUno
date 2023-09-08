72d971777d039c88f43cf9f418be5429ad613acc
AranzaMich <aranzamichelle1@hotmail.com>
2023-09-07 21:24:16 -0600

    Cambios 1

--- Proyecto.py
+++ Proyecto.py
@@ -1,12 +1,8 @@
-
+print ("Voy a modificar a partir de esta línea, Evelyn")
+print ("Archivo Modificado")
+print (" [: ")
   
 
-
-
-
-
-
-  
 print ("Voy a modificar a partir de esta línea, atte: Aranza")
 print ("Archivo modificado nuevamente")
 print (" C: ")
